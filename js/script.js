6dde6e3e387780480a5cd88f94556dfcd1131053
Thomas Mielke <101403983+Scryptir@users.noreply.github.com>
2023-12-18 13:00:29 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+
+var clickedSquare;
+var counterVariable;
+
