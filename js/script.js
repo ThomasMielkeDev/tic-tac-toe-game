bf26562b07d26cd215b181265719326105992dda
Thomas Mielke <temielke@gmail.com>
2024-01-10 09:31:07 -0500

    Finished the Tic-Tac-Toe Game
    Added a draw condition, added an option to replay once game is over, and fixed style of information text.

--- js/script.js
+++ js/script.js
@@ -9,17 +9,20 @@ var eight = document.getElementById("eight");
 var nine = document.getElementById("nine");
 
 let counter = 0;
+let turn = 1;
 
 function markOne() {
     if(counter === 0) {
         one.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         one = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1) {
         one.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         one = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -29,11 +32,13 @@ function markTwo() {
         two.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         two = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         two.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         two = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -43,11 +48,13 @@ function markThree() {
         three.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         three = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         three.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         three = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -57,11 +64,13 @@ function markFour() {
         four.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         four = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         four.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         four = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -71,11 +80,13 @@ function markFive() {
         five.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         five = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         five.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         five = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -85,11 +96,13 @@ function markSix() {
         six.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         six = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         six.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         six = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -99,11 +112,13 @@ function markSeven() {
         seven.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         seven = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         seven.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         seven = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -113,11 +128,13 @@ function markEight() {
         eight.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         eight = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         eight.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         eight = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
@@ -127,58 +144,92 @@ function markNine() {
         nine.innerHTML = '<img src="./images/x-image.png" alt="x-image"></img>';
         nine = "x";
         counter = 1;
+        oNext();
         checkWinCondition();
-    } else {
+    } else if (counter === 1){
         nine.innerHTML = '<img src="./images/o-image.png" alt="x-image"></img>';
         nine = "o";
         counter = 0;
+        xNext();
         checkWinCondition();
     }
 }
 
 function checkWinCondition() {
     if(one === 'x' && two === 'x' && three === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (one === 'o' && two === 'o' && three === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if(four === 'x' && five === 'x' && six === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (four === 'o' && five === 'o' && six === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>'
     } else if(seven === 'x' && eight === 'x' && nine === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (seven === 'o' && eight === 'o' && nine === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if(one === 'x' && four === 'x' && seven === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (one === 'o' && four === 'o' && seven === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if(two === 'x' && five === 'x' && eight === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (two === 'o' && five === 'o' && eight === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if(three === 'x' && six === 'x' && nine === 'x'){
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (three === 'o' && six === 'o' && nine === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (one === 'x' && five === 'x' && nine === 'x') {
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (one === 'o' && five === 'o' && nine === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (three === 'x' && five === 'x' && seven === 'x') {
-        document.getElementById('information-container').innerHTML = 'X WINS!'
+        document.getElementById('information-container').innerHTML = '<p>X WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
     } else if (three === 'o' && five === 'o' && seven === 'o') {
-        document.getElementById('information-container').innerHTML = 'O WINS!'
+        document.getElementById('information-container').innerHTML = '<p>O WINS!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
+    } else if (turn === 9) {
+        document.getElementById('information-container').innerHTML = '<p>DRAW GAME!</p> <a href="./index.html">Play Again!</a>';
+        removeMarker();
+    }
+    else {
+        turn++;
     }
 }
 
+function removeMarker() {
+    counter = 2;
+}
 
-one.addEventListener('click', markOne, checkWinCondition);
-two.addEventListener('click', markTwo, checkWinCondition);
-three.addEventListener('click', markThree, checkWinCondition);
-four.addEventListener('click', markFour, checkWinCondition);
-five.addEventListener('click', markFive, checkWinCondition);
-six.addEventListener('click', markSix, checkWinCondition);
-seven.addEventListener('click', markSeven, checkWinCondition);
-eight.addEventListener('click', markEight, checkWinCondition);
-nine.addEventListener('click', markNine, checkWinCondition);
+function xNext() {
+    document.getElementById('information-container').innerHTML = `<p>X's Turn!</p>`;
+}
+
+function oNext() {
+    document.getElementById('information-container').innerHTML = `<p>O's Turn!</p>`;
+}
+
+one.addEventListener('click', markOne);
+two.addEventListener('click', markTwo);
+three.addEventListener('click', markThree);
+four.addEventListener('click', markFour);
+five.addEventListener('click', markFive);
+six.addEventListener('click', markSix);
+seven.addEventListener('click', markSeven);
+eight.addEventListener('click', markEight);
+nine.addEventListener('click', markNine);
